010cf1a62efe9739be820413e75f9065fb26e601
m-malanowski <info@mateusz-malanowski.pl>
2023-02-10 14:39:29 +0100

    initial commit

---
+++ config/database.js
@@ -0,0 +1,53 @@
+// const path = require('path');
+//
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+const path = require("path");
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL)
+
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            },
+          },
+          options: {
+//             ssl: false,
+            ssl: env.bool('DATABASE_SSL', false),
+          }
+        }
+      }
+    }
+  }
+
+  return{
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  }
+};
