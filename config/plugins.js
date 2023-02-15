de1c1c4977d8718e2a72bbc289e7cfe5b7c4f23b
m-malanowski <info@mateusz-malanowski.pl>
2023-02-15 19:20:23 +0100

    cloudinary plugin added

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,40 @@
-// module.exports = ({ env }) => {
-//   if (env('NODE_ENV') === 'production'){
-//     return {
-//       upload: {
-//         provider: 'cloudinary',
-//         providerOptions: {
-//           cloud_name: env('CLOUDINARY_NAME'),
-//           api_key: env('CLOUDINARY_KEY'),
-//           api_secret: env('CLOUDINARY_SECRET'),
-//         },
-//         actionOptions: {
-//           upload: {},
-//           delete: {}
-//         }
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production'){
+    return {
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {}
+        }
+      },
+    }
+  }
+  return{
+
+  }
+}
+
+// module.exports = ({ env }) => ({
+//   // ...
+//   upload: {
+//     config: {
+//       provider: 'cloudinary',
+//       providerOptions: {
+//         cloud_name: env('CLOUDINARY_NAME'),
+//         api_key: env('CLOUDINARY_KEY'),
+//         api_secret: env('CLOUDINARY_SECRET'),
 //       },
-//     }
-//   }
-//   return{
-//
-//   }
-// }
+//       actionOptions: {
+//         upload: {},
+//         delete: {},
+//       },
+//     },
+//   },
+//   // ...
+// });
